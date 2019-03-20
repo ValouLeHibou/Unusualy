85caee61f09403dd2f4837e2c73fa1303b87f913
Valentin <valentin@mindmatcher.org>
2019-03-20 17:40:44 +0100

    usless push

--- app.py
+++ app.py
@@ -3,10 +3,10 @@ from flask import Flask, render_template
 app = Flask(__name__)
 app.debug = True
 
-@app.route('/<name>')
+@app.route('/')
 def index(name=None):
     return render_template('index.html', name=name)
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
