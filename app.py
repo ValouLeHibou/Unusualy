9ba2fb659ba8dd74c38d6ee87ea096a7f33cd3e4
ValouLeHibou <valentin.guilbaud@eemi.com>
2019-03-20 07:03:24 -0700

    push flask

--- app.py
+++ app.py
@@ -1,21 +1,11 @@
 from flask import Flask
-from flask import render_template
-
 
 app = Flask(__name__)
-app.debug = True
 
 
-@app.route('/<name>')
-def index(name=None):
-    return render_template('index.html', name=name)
-
-
-@app.route('/contact/')
-def contact():
-    mail = "jean@bon.fr"
-    tel = "01 23 45 67 89"
-    return "<h1>Mail: {} --- Tel: {}</h1>".format(mail, tel)
+@app.route('/')
+def hello_world():
+    return 'Hello World!'
 
 
 if __name__ == '__main__':
